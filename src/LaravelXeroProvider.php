702a6243b9e7cbd9d15b73832e340dd8455a2457
bathulah <bathulahmahir@gmail.com>
2018-02-02 18:43:12 +0800

    first commit

---
+++ src/LaravelXeroProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Batmahir\LaravelXero;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelXeroProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    //Batmahir\LaravelXero\LaravelXeroProvider
+}
