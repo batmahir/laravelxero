3dc8f47ff239501e0954cee206789c2485d7e268
bathulah <bathulahmahir@gmail.com>
2018-02-05 15:34:42 +0800

    add config file

--- src/LaravelXeroServiceProvider.php
+++ src/LaravelXeroServiceProvider.php
@@ -13,7 +13,9 @@ class LaravelXeroServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->publishes([
+            __DIR__.'/config.php' => config_path('xerobat.php'),
+        ]);
     }
 
     /**
