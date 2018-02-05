1661b0b88313f51ea10122c7464938f3e5802226
bathulah <bathulahmahir@gmail.com>
2018-02-05 11:40:56 +0800

    update

---
+++ src/LaravelXeroServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Batmahir\Laravelxero;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelXeroServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
