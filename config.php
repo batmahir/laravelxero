b8c6c5f578ae3cba97dd17ea53f10dea927bbeef
bathulah <bathulahmahir@gmail.com>
2018-02-05 17:29:15 +0800

    update config file

--- config.php
+++ config.php
@@ -1,20 +1,35 @@
 <?php
 
-return array(
-
+return [
     /*
     |--------------------------------------------------------------------------
-    | Image Driver
+    | OAuth
     |--------------------------------------------------------------------------
-    |
-    | Intervention Image supports "GD Library" and "Imagick" to process images
-    | internally. You may choose one of them according to your PHP
-    | configuration. By default PHP's "GD Library" implementation is used.
-    |
-    | Supported: "gd", "imagick"
-    |
     */
+    'oauth' => [
+        /*
+        |--------------------------------------------------------------------------
+        | Callback URL
+        |--------------------------------------------------------------------------
+        |
+        | Provide a callback URL
+        |
+        */
+        'callback' => env('XERO_CALLBACK', ''),
 
-    'driver' => 'gd'
+        /*
+        |--------------------------------------------------------------------------
+        | Xero application authentication
+        |--------------------------------------------------------------------------
+        |
+        | Before using this service, you'll need to register an applicatin via
+        | the Xero developer website. When setting up your application, take
+        | note of the consumer key and shared secret, as well as the
+        | application type (this package support for public only).
+        |
+        */
+        'consumer_key' => env('XERO_CUSTOMER_KEY', ''),
+        'consumer_secret' => env('XERO_CUSTOMER_SECRET', ''),
 
-);
+    ],
+];
